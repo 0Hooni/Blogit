f7c4257d04af84b0f223740f5fc19601d75ed6ae
0Hooni <thddudgns972@gmail.com>
2025-05-28 00:32:25 +0900

    feat/#14: 로그인 화면 리팩터링 및 리다리렉트 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,12 @@
+import { useAuth } from "@/src/contexts/AuthContext";
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/(login)" />;
+  const { user, isInitialized } = useAuth();
+
+  if (!isInitialized) return null;
+
+  if (user) return <Redirect href="/(tabs)" />;
+
+  return <Redirect href="/login" />;
 }
