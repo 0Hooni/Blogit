849ea145ee3b969336165a17a0c8b4ab700a849f
0Hooni <thddudgns972@gmail.com>
2025-05-27 22:52:21 +0900

    feat/#14: 로그인시 전환될 탭 화면 임시 구현

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: "홈" }} />
+      <Tabs.Screen name="setting" options={{ title: "설정" }} />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export default function HomeScreen() {
+  return (
+    <Container>
+      <Title>{"Blogit\n설정 화면"}</Title>
+    </Container>
+  );
+}
