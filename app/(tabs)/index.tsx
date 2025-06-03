1479d50ba862d4dfae759685d140c99117b65fd5
0Hooni <thddudgns972@gmail.com>
2025-06-03 23:30:05 +0900

    feat/#25: 포스트 개수 화면 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
+import { textStyle } from "@/styles/textStyle";
 import { styled } from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
+  padding: 20px 0px;
+`;
+
+const PostTitle = styled.Text`
+  ${textStyle("body1")};
+  color: ${({ theme }) => theme.colors.foreground};
+  padding: 20px;
+  border-width: 1px;
+  border-color: ${({ theme }) => theme.colors.border};
+  border-radius: 12px;
 `;
 
 const Title = styled.Text`
@@ -16,7 +27,7 @@ const Title = styled.Text`
 export default function HomeScreen() {
   return (
     <Container>
-      <Title>{"Blogit\n홈 화면"}</Title>
+      <PostTitle>{"포스트 갯수"}</PostTitle>
     </Container>
   );
 }
