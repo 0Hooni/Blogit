340c0d314957878e6e68d40bc15031df23e441cc
0Hooni <thddudgns972@gmail.com>
2025-06-05 00:50:31 +0900

    feat/#25: 대시보드 컨텐츠 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { ThemedIcon } from "@/src/components/ThemedIcon";
 import { textStyle } from "@/styles/textStyle";
 import { styled } from "styled-components/native";
 
@@ -12,19 +13,17 @@ const Container = styled.View`
 const CardSection = styled.View`
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
-  width: 100%;
   padding: 16px;
   gap: 16px;
 `;
 
 const CardView = styled.View`
+  flex: 1;
   flex-direction: column;
   gap: 8px;
-  padding: 24px;
+  padding: 16px;
   border-radius: 12px;
   border: 1px ${({ theme }) => theme.colors.border};
-  width: 50%;
 `;
 
 const CardTitle = styled.Text`
@@ -35,6 +34,50 @@ const CardTitle = styled.Text`
 const CardContent = styled.Text`
   ${textStyle("display")};
   color: ${({ theme }) => theme.colors.foreground};
+`;
+
+const SectionContainer = styled.View`
+  flex-direction: column;
+  width: 100%;
+`;
+
+const SectionTitle = styled.Text`
+  ${textStyle("title")};
+  color: ${({ theme }) => theme.colors.foreground};
+  padding: 0px 16px;
+`;
+
+const SectionContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 16px;
+  width: 100%;
+  padding: 12px 16px;
+`;
+
+const SectionIcon = styled.View`
+  width: 48px;
+  height: 48px;
+  background-color: ${({ theme }) => theme.colors.muted};
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SectionItem = styled.View`
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const SectionItemTitle = styled.Text`
+  ${textStyle("body1")};
+  color: ${({ theme }) => theme.colors.foreground};
+`;
+
+const SectionItemContent = styled.Text`
+  ${textStyle("label")};
+  color: ${({ theme }) => theme.colors.mutedForeground};
 `;
 
 export default function HomeScreen() {
@@ -50,6 +93,75 @@ export default function HomeScreen() {
           <CardContent>{"23"}</CardContent>
         </CardView>
       </CardSection>
+
+      <SectionContainer>
+        <SectionTitle>{"Repository Status"}</SectionTitle>
+
+        <SectionContent>
+          <SectionIcon>
+            <ThemedIcon
+              name="git-branch-outline"
+              size={24}
+              variant="foreground"
+            />
+          </SectionIcon>
+          <SectionItem>
+            <SectionItemTitle>{"Repository name"}</SectionItemTitle>
+            <SectionItemContent>
+              {"Last updated 2 hours ago"}
+            </SectionItemContent>
+          </SectionItem>
+        </SectionContent>
+      </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle>{"Recent Posts"}</SectionTitle>
+
+        <SectionContent>
+          <SectionIcon>
+            <ThemedIcon
+              name="document-text-outline"
+              size={24}
+              variant="foreground"
+            />
+          </SectionIcon>
+          <SectionItem>
+            <SectionItemTitle>{"Post Title"}</SectionItemTitle>
+            <SectionItemContent>{"Published 2 min ago"}</SectionItemContent>
+          </SectionItem>
+        </SectionContent>
+      </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle>{"Recent Comments"}</SectionTitle>
+
+        <SectionContent>
+          <SectionIcon>
+            <ThemedIcon
+              name="chatbubble-ellipses-outline"
+              size={24}
+              variant="foreground"
+            />
+          </SectionIcon>
+          <SectionItem>
+            <SectionItemTitle>{"Comment content"}</SectionItemTitle>
+            <SectionItemContent>{"Commented now"}</SectionItemContent>
+          </SectionItem>
+        </SectionContent>
+        <SectionContent>
+          <SectionIcon>
+            <ThemedIcon
+              name="chatbubble-ellipses-outline"
+              size={24}
+              variant="foreground"
+            />
+          </SectionIcon>
+          <SectionItem>
+            <SectionItemTitle>{"Comment content"}</SectionItemTitle>
+            <SectionItemContent>{"Commented now"}</SectionItemContent>
+          </SectionItem>
+        </SectionContent>
+      </SectionContainer>
     </Container>
   );
 }
