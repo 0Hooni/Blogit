f1f731176b4073039f55069518f147954462fb2f
0Hooni <thddudgns972@gmail.com>
2025-06-14 16:24:44 +0900

    feat/#25: 코멘트 갯수 카드 컴포넌트 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { CommentCountCard } from "@/src/components/CommentCountCard";
 import { PostCountCard } from "@/src/components/PostCountCard";
 import RecentCommentsView from "@/src/components/RecentCommentsView";
 import RecentPostsView from "@/src/components/RecentPostsView";
@@ -24,25 +25,6 @@ const CardSection = styled.View`
   gap: 16px;
 `;
 
-const CardView = styled.View`
-  flex: 1;
-  flex-direction: column;
-  gap: 8px;
-  padding: 16px;
-  border-radius: 12px;
-  border: 1px ${({ theme }) => theme.colors.border};
-`;
-
-const CardTitle = styled.Text`
-  ${textStyle("section")};
-  color: ${({ theme }) => theme.colors.foreground};
-`;
-
-const CardContent = styled.Text`
-  ${textStyle("display")};
-  color: ${({ theme }) => theme.colors.foreground};
-`;
-
 const SectionContainer = styled.View`
   flex-direction: column;
   width: 100%;
@@ -54,49 +36,13 @@ const SectionTitle = styled.Text`
   padding: 0px 16px;
 `;
 
-const SectionContent = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 16px;
-  width: 100%;
-  padding: 12px 16px;
-`;
-
-const SectionIcon = styled.View`
-  width: 48px;
-  height: 48px;
-  background-color: ${({ theme }) => theme.colors.muted};
-  border-radius: 8px;
-  justify-content: center;
-  align-items: center;
-`;
-
-const SectionItem = styled.View`
-  flex-direction: column;
-  align-items: flex-start;
-`;
-
-const SectionItemTitle = styled.Text`
-  ${textStyle("body1")};
-  color: ${({ theme }) => theme.colors.foreground};
-`;
-
-const SectionItemContent = styled.Text`
-  ${textStyle("label")};
-  color: ${({ theme }) => theme.colors.mutedForeground};
-`;
-
 export default function HomeScreen() {
   return (
     <Container showsVerticalScrollIndicator={false}>
       <ContentWrapper>
         <CardSection>
           <PostCountCard />
-          <CardView>
-            <CardTitle>{"Comments"}</CardTitle>
-            <CardContent>{"23"}</CardContent>
-          </CardView>
+          <CommentCountCard />
         </CardSection>
 
         <SectionContainer>
