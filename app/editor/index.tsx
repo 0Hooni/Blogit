67ab6bc4c162ed284ad01354570a55c90e0691b3
0Hooni <thddudgns972@gmail.com>
2025-06-16 01:48:46 +0900

    feat/#30: 간단한 에디터 화면 구현

--- app/editor/index.tsx
+++ app/editor/index.tsx
@@ -1,10 +1,32 @@
+import { textStyle } from "@/src/styles/textStyle";
 import { theme } from "@/src/styles/theme";
-import { Text, useColorScheme } from "react-native";
+import { useColorScheme } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
+  padding: 16px;
+  gap: 16px;
+`;
+
+const TitleInput = styled.TextInput`
+  ${textStyle("title")};
+  color: ${({ theme }) => theme.colors.foreground};
+  background-color: ${({ theme }) => theme.colors.primaryForeground};
+  height: 72px;
+  padding: 16px;
+  border-radius: 12px;
+`;
+
+const ContentInput = styled.TextInput`
+  ${textStyle("body2")};
+  color: ${({ theme }) => theme.colors.foreground};
+  min-height: 120px;
+  text-align-vertical: top;
+  background-color: ${({ theme }) => theme.colors.primaryForeground};
+  border-radius: 12px;
+  padding: 16px;
 `;
 
 export default function Editor() {
@@ -13,7 +35,15 @@ export default function Editor() {
 
   return (
     <Container>
-      <Text style={{ color: selectedTheme.colors.foreground }}>Editor</Text>
+      <TitleInput
+        style={{ color: selectedTheme.colors.foreground }}
+        placeholder="Title"
+      />
+      <ContentInput
+        multiline={true}
+        style={{ color: selectedTheme.colors.foreground }}
+        placeholder="Content"
+      />
     </Container>
   );
 }
